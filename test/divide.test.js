f4f8aa5e1f20f6a2c5d8f0dbb8cc9e23903dd115
Zenzi Ali <zeeathome935@gmail.com>
2020-08-11 19:02:05 -0400

    mocha exercise 1

---
+++ test/divide.test.js
@@ -0,0 +1,11 @@
+const divide = require('../index.js');
+
+describe('Divide fuction', ()=> {
+  it('should divide positive integers correctly', () => {
+
+  });
+
+  it('should throw and error when divide by zero', () => {
+
+  });
+});
